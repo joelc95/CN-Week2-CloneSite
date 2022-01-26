bc4d099655d8b44214395ec2a0ef9952c16e08e1
joelc95 <joelconalty@gmail.com>
2022-01-26 10:17:32 +0000

    added search bar in nav on scroll event

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,14 @@
 let mainNav = document.getElementById("main-nav-bar");
-
+let mainNavSearch = document.getElementById("main-nav-search");
 
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         mainNav.style.backgroundColor = "#232a34";
+        mainNavSearch.style.display = "flex";
     } else if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0) {
-        mainNav.style.backgroundColor = "";
+        mainNav.style.backgroundColor = ""
+        mainNavSearch.style.display = "none"
     }
 }
